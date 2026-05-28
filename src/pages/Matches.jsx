bf82ef4aba8e7e0cd1528f1578f73ae981f2846a
NSJL-dev <nsjl@nsjl.nl>
2026-05-28 16:18:02 +0200

    Add files via upload

---
+++ src/pages/Matches.jsx
@@ -0,0 +1,21 @@
+
+export default function Matches(){
+  return(
+    <div className="container" style={{paddingTop:'120px',paddingBottom:'120px'}}>
+      <h1 className="section-title">Wedstrijden</h1>
+
+      <div className="grid">
+        <div className="card">
+          <h2>NSJL vs Dart Devils</h2>
+          <p style={{marginTop:'12px'}}>Vrijdag - 20:00</p>
+          <p style={{marginTop:'12px'}}>Live score: 4 - 3</p>
+        </div>
+
+        <div className="card">
+          <h2>180 Hunters vs NSJL</h2>
+          <p style={{marginTop:'12px'}}>Volgende week</p>
+        </div>
+      </div>
+    </div>
+  )
+}
